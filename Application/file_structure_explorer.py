0dcaa8dfa414ddcf03919718129457e03e08016c
Nils Jensen <nilsejensen28@gmail.com>
2025-02-26 19:37:52 +0100

    Further development

--- Application/file_structure_explorer.py
+++ Application/file_structure_explorer.py
@@ -6,9 +6,12 @@ It generates a json file that represents the folder structure of the given direc
 import os
 import json
 import logging
+import subprocess
+from abc import ABC, abstractmethod
 
 LOGGING_LEVEL = logging.INFO
-
+LATEX_TEMPLATE_PATH = 'Templates/folder_structure_template.tex'
+LABEL_TYPES = ["hanging", "sticker", "outside_folder", "inside_folder"]
 
 '''
 This class represents a folder. It is a node in the folder tree.
@@ -16,14 +19,14 @@ This class represents a folder. It is a node in the folder tree.
 class Folder:
     name : str #Name of the folder
     id : str #ID of the folder
-    physical_location : str #Physical location of the folder
+    label_type : str #Physical location of the folder
     subfolders : list #List of subfolders of the folder
     parent = None  #Parent folder of the folder
 
-    def __init__(self, name : str, id : str, physical_location="", parent=None):
+    def __init__(self, name : str, id : str, label_type="", parent=None):
         self.name = name
         self.id = id
-        self.physical_location = physical_location
+        self.label_type = label_type
         self.subfolders = []
         self.parent = parent
 
@@ -36,6 +39,20 @@ class Folder:
         '''Remove a subfolder from the folder'''
         self.subfolders.remove(subfolder)
         subfolder.parent = None
+
+    def perform_check(self) -> bool:
+        '''Perform a check on the folder'''
+        if not self.name:
+            logging.error("Folder name is empty")
+            return False
+        if not self.id:
+            logging.error("Folder ID is empty")
+            return False
+        if not self.label_type in LABEL_TYPES:
+            logging.error(f"Invalid label type {self.label_type}")
+            return False
+        return True
+    
 ''' 
 
 This class represents a folder tree. It is a tree structure that represents the folder structure of a given directory.
@@ -111,6 +128,53 @@ class FolderTree:
             ]
         }
         return json
+    
+    def generate_latex_export(self, output_file : os.path, generate_pdf=True):
+        '''Generate a latex file that represents the folder tree'''
+        with open(LATEX_TEMPLATE_PATH, 'r') as file:
+            latex_template = file.read()
+        latex_template = latex_template.replace('##FOLDER_STRUCTURE##', self._generate_latex_tree_recursive(self.root))
+        with open(output_file, 'w') as file:
+            file.write(latex_template)
+        if generate_pdf:
+            subprocess.run(['xelatex', output_file, '-interaction=nonstopmode'])
+            logging.info(f"PDF generated and saved to {output_file.replace('.tex', '.pdf')}")
+            subprocess.run(['rm', output_file.replace('.tex', '.aux')])
+            if not LOGGING_LEVEL == logging.DEBUG:
+                subprocess.run(['rm', output_file.replace('.tex', '.log')])
+
+    def _generate_latex_tree_recursive(self, folder : Folder) -> str:
+        name = "\\textbf{" + folder.id + "} - " + folder.name
+        content = f"[{name} \n"
+        for subfolder in folder.subfolders:
+            content += self._generate_latex_tree_recursive(subfolder)
+        content += "]"
+        return content
+    
+    def generate_labels(self, output_dir : os.path):
+        '''Generate labels for the folders'''
+        labels = {}
+
+
+class AbstractLabel(ABC):
+    folder: Folder
+    template_file: str
+
+    def __init__(self, folder: Folder, type: str):
+        self.folder = folder
+    
+    @abstractmethod
+    def to_latex(self) -> str:
+        pass
+
+class HangingFolderLabel(AbstractLabel):
+    template_file = 'Templates/hanging_folder_label_template.tex'
+
+    def to_latex(self) -> str:
+        if self.folder.parent:
+            return f"\\customlabel{{{self.folder.id}}}{{{self.folder.parent.name}}}{{{self.folder.name}}}"
+        return f"\\customlabel{{{self.folder.id}}}{{}}{{{self.folder.name}}}"
+
        
 def main():
     folder_tree = FolderTree()
@@ -119,6 +183,10 @@ def main():
     folder_tree_2 = FolderTree()
     folder_tree_2.generate_folder_tree_from_path("0_Test")
     print(folder_tree_2.generate_folder_tree_json())
+    folder_tree_2.generate_latex_export("folder_structure.tex")
+    folder = Folder("Test", "0")
+    label = HangingFolderLabel(folder, "hanging")
+    print(label.to_latex())
 
 if __name__ == '__main__':
     logging.basicConfig(level=LOGGING_LEVEL)
